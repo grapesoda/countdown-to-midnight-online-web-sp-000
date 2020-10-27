3d2a4f5705735e98c4f6740a7b8c8952238f3659
Tres Tarwater <trest03@gmail.com>
2020-10-27 19:16:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,10 @@
 #write your code here
 
 def countdown(number)
-  number = 10
-  while number > 0 
-    puts "#{number} SECOND(S)!"
-    number -= 1
+ x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
   end
-  
-  puts "HAPPY NEW YEAR!"
-  
 end
- 
+ puts "HAPPY NEW YEAR!"
